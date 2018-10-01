68541c03f690e5eedb4322e84d69b2db42abd95d
cstielper <chris@mm4solutions.com>
2018-10-01 13:57:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Weather App Back End
+
+![Version 1.0](https://img.shields.io/badge/Version-1.0-brightgreen.svg)
+
+Back end proxy for the [Dark Sky REST API](https://darksky.net/dev/docs).
---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const fetch = require('node-fetch');
+
+require('dotenv').config();
+
+const app = express();
+const dsApiKey = process.env.DS_API_KEY;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  const lat = req.query.lat;
+  const lng = req.query.lng;
+  const units = req.query.units;
+
+  if (lat && lng && units) {
+    res.setHeader('Content-Type', 'application/json');
+    fetch(
+      `https://api.darksky.net/forecast/${dsApiKey}/${lat},${lng}?units=${units}`
+    )
+      .then(response => response.json())
+      .then(data => res.end(JSON.stringify(data)))
+      .catch(err => console.error(err));
+  } else {
+    res.status(400).json({ error: "We're sorry, but there was a problem." });
+  }
+});
+
+app.listen('3001', '127.0.0.1');
+console.log('Listening to port 3001');
